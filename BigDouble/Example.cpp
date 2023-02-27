61193af02b047cabd9a8ad449048f98684fb8fa2
Soskar1 <oskar.vistorskis@gmail.com>
2023-02-27 16:29:41 +0200

    rewriting

--- BigDouble/Example.cpp
+++ BigDouble/Example.cpp
@@ -1,21 +1,6 @@
 ﻿#include <iostream>
 #include <string>
-#include "BigDouble.h"
 
-using namespace std;
+int main() {
 
-int main()
-{
-	string firstBuffer;
-	string secondBuffer;
-
-	cin >> firstBuffer;
-	cin >> secondBuffer;
-
-	Big::BigDouble first(firstBuffer);
-	Big::BigDouble second(secondBuffer);
-
-	Big::BigDouble third = first + second;
-
-	cout << third.ToString();
 }

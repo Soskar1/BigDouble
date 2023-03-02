1b5cd70bf6f9f036b70bbb10a47e2ff3b6a6121f
Soskar1 <oskar.vistorskis@gmail.com>
2023-03-02 22:26:24 +0200

    log file for displaying tests

--- BigDouble/Tests.cpp
+++ BigDouble/Tests.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <cassert>
 #include <string>
+#include <fstream>
 
 #include "BigDouble.h"
 #include "BigInt.h"
+
+std::ofstream fout("log.txt");
 
 int test = 0;
 
@@ -15,7 +18,7 @@ void BigIntAdditionTest(const std::string& firstArg, const std::string& secondAr
 
 	Big::BigInt third = first + second;
 
-	std::cout << test << "# result: " << third.ToString() << std::endl;
+	fout << test << "# result: " << third.ToString() << std::endl;
 
 	assert(third.ToString() == result);
 }
@@ -28,7 +31,7 @@ void BigDoubleAdditionTest(const std::string& firstArg, const std::string& secon
 
 	Big::BigDouble third = first + second;
 
-	std::cout << test << "# result: " << third.ToString() << std::endl;
+	fout << test << "# result: " << third.ToString() << std::endl;
 
 	assert(third.ToString() == result);
 }
@@ -41,7 +44,7 @@ void BigIntSubtractTest(const std::string& firstArg, const std::string& secondAr
 
 	Big::BigInt third = first - second;
 
-	std::cout << test << "# result: " << third.ToString() << std::endl;
+	fout << test << "# result: " << third.ToString() << std::endl;
 
 	assert(third.ToString() == result);
 }
@@ -54,7 +57,7 @@ void BigDoubleSubtractTest(const std::string& firstArg, const std::string& secon
 
 	Big::BigDouble third = first - second;
 
-	std::cout << test << "# result: " << third.ToString() << std::endl;
+	fout << test << "# result: " << third.ToString() << std::endl;
 
 	assert(third.ToString() == result);
 }
@@ -67,7 +70,7 @@ void BigIntLessTest(const std::string& firstArg, const std::string& secondArg, b
 
 	bool less = first < second;
 
-	std::cout << test << "# result: " << less << std::endl;
+	fout << test << "# result: " << less << std::endl;
 
 	assert(less == result);
 }
@@ -80,7 +83,7 @@ void BigDoubleLessTest(const std::string& firstArg, const std::string& secondArg
 
 	bool less = first < second;
 
-	std::cout << test << "# result: " << less << std::endl;
+	fout << test << "# result: " << less << std::endl;
 
 	assert(less == result);
 }
@@ -93,30 +96,30 @@ void BigIntEqualTest(const std::string& firstArg, const std::string& secondArg, 
 
 	bool equal = first == second;
 
-	std::cout << test << "# result: " << equal << std::endl;
+	fout << test << "# result: " << equal << std::endl;
 
 	assert(equal == result);
 }
 
 void main() {
-	//std::cout << "BigInt Addition Tests: \n";
-	//BigIntAdditionTest("2", "5", "7");
-	//BigIntAdditionTest("7", "3", "10");
-	//BigIntAdditionTest("1999", "1", "2000");
-	//BigIntAdditionTest("999", "1", "1000");
-
-	//test = 0;
-	//std::cout << "---------\n";
-	//std::cout << "BigDouble Addition Tests: \n";
-	//BigDoubleAdditionTest("0.2", "0.5", "0.7");
-	//BigDoubleAdditionTest("0.031", "0.071", "0.102");
-	//BigDoubleAdditionTest("0.93", "0.12", "1.05");
-	//BigDoubleAdditionTest("0.99999", "0.00001", "1.0");
-	//BigDoubleAdditionTest("999.99999", "0.00001", "1000.0");
+	fout << "BigInt Addition Tests: \n";
+	BigIntAdditionTest("2", "5", "7");
+	BigIntAdditionTest("7", "3", "10");
+	BigIntAdditionTest("1999", "1", "2000");
+	BigIntAdditionTest("999", "1", "1000");
 
 	test = 0;
-	std::cout << "---------\n";
-	std::cout << "BigInt Subtract Tests: \n";
+	fout << "---------\n";
+	fout << "BigDouble Addition Tests: \n";
+	BigDoubleAdditionTest("0.2", "0.5", "0.7");
+	BigDoubleAdditionTest("0.031", "0.071", "0.102");
+	BigDoubleAdditionTest("0.93", "0.12", "1.05");
+	BigDoubleAdditionTest("0.99999", "0.00001", "1.0");
+	BigDoubleAdditionTest("999.99999", "0.00001", "1000.0");
+
+	test = 0;
+	fout << "---------\n";
+	fout << "BigInt Subtract Tests: \n";
 	BigIntSubtractTest("8", "5", "3");
 	BigIntSubtractTest("999", "187", "812");
 	BigIntSubtractTest("10", "2", "8");
@@ -132,8 +135,8 @@ void main() {
 	BigIntSubtractTest("5", "5", "0");
 
 	test = 0;
-	std::cout << "---------\n";
-	std::cout << "BigDouble Subtract Tests: \n";
+	fout << "---------\n";
+	fout << "BigDouble Subtract Tests: \n";
 	BigDoubleSubtractTest("0.5", "0.3", "0.2");
 	BigDoubleSubtractTest("0.537", "0.121", "0.416");
 	BigDoubleSubtractTest("0.47", "0.18", "0.29");
@@ -150,33 +153,34 @@ void main() {
 	BigDoubleSubtractTest("5.7", "5.7", "0.0");
 	BigDoubleSubtractTest("1.0", "10000.1", "-9999.1");
 
-	//test = 0;
-	//std::cout << "---------\n";
-	//std::cout << "BigInt < Tests: \n";
-	//BigIntLessTest("152", "372", true);
-	//BigIntLessTest("1", "0", false);
-	//BigIntLessTest("25", "1", false);
-	//BigIntLessTest("48", "4014158", true);
-	//BigIntLessTest("4014157", "4014158", true);
-	//BigIntLessTest("99999", "99999", false);
+	test = 0;
+	fout << "---------\n";
+	fout << "BigInt < Tests: \n";
+	BigIntLessTest("152", "372", true);
+	BigIntLessTest("1", "0", false);
+	BigIntLessTest("25", "1", false);
+	BigIntLessTest("48", "4014158", true);
+	BigIntLessTest("4014157", "4014158", true);
+	BigIntLessTest("99999", "99999", false);
 
-	//test = 0;
-	//std::cout << "---------\n";
-	//std::cout << "BigDouble < Tests: \n";
-	//BigDoubleLessTest("2.0", "5.6", true);
-	//BigDoubleLessTest("7.2", "5.6", false);
-	//BigDoubleLessTest("777.2", "777.6", true);
-	//BigDoubleLessTest("1.8468", "1.84681", true);
-	//BigDoubleLessTest("1.8468", "1.845", false);
-	//BigDoubleLessTest("1.8468", "1.846", false);
-	//BigDoubleLessTest("0.001", "0.01", true);
-	//BigDoubleLessTest("0.47", "0.18", false);
+	test = 0;
+	fout << "---------\n";
+	fout << "BigDouble < Tests: \n";
+	BigDoubleLessTest("2.0", "5.6", true);
+	BigDoubleLessTest("7.2", "5.6", false);
+	BigDoubleLessTest("777.2", "777.6", true);
+	BigDoubleLessTest("1.8468", "1.84681", true);
+	BigDoubleLessTest("1.8468", "1.845", false);
+	BigDoubleLessTest("1.8468", "1.846", false);
+	BigDoubleLessTest("0.001", "0.01", true);
+	BigDoubleLessTest("0.47", "0.18", false);
 
-	//test = 0;
-	//std::cout << "---------\n";
-	//std::cout << "BigInt == Tests: \n";
-	//BigIntEqualTest("100", "100", true);
-	//BigIntEqualTest("1", "100", false);
-	//BigIntEqualTest("14684", "100", false);
+	test = 0;
+	fout << "---------\n";
+	fout << "BigInt == Tests: \n";
+	BigIntEqualTest("100", "100", true);
+	BigIntEqualTest("1", "100", false);
+	BigIntEqualTest("14684", "100", false);
 
+	fout.close();
 }
